a8e05c3a972f3451c91f55e95d6f09f949fe5f37
Dan Boger <zigdon@irc.peeron.com>
2010-04-06 17:21:45 -0700

    Add missing users table

--- ROBOT9000.sql
+++ ROBOT9000.sql
@@ -25,3 +25,23 @@ CREATE TABLE IF NOT EXISTS `lines` (
   PRIMARY KEY  (`id`),
   KEY `msg` (`msg`(32))
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `mask` varchar(255) default NULL,
+  `nick` varchar(64) NOT NULL default '',
+  `timeout_power` int(10) unsigned NOT NULL default '0',
+  `banned_until` int(10) unsigned NOT NULL default '0',
+  `ban_type` char(1) NOT NULL default 'v',
+  `lines_talked` int(10) unsigned NOT NULL default '0',
+  `total_bans` int(10) unsigned NOT NULL default '0',
+  `word_count` int(10) unsigned NOT NULL default '0',
+  `last_talk` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `user_status` tinyint(3) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `mask` (`mask`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
